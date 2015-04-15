6a1cfb1f2562a4ce7e82049b36a93f3144d4fc26
Shou Takenaka <sasaminn@gmail.com>
2015-04-15 01:28:59 +0000

    Move some method definitions to cellgraph.rb

--- lib/cellgraph.rb
+++ lib/cellgraph.rb
@@ -6,4 +6,21 @@ require 'cellgraph/cellgraph_callbacks'
 require 'cellgraph/acts_as_cellgraph'
 
 module Cellgraph
+  # Return the current configuration or a new one.
+  def self.configuration
+    @configuration ||= Cellgraph::Configuration.new
+  end
+
+  # Resets the configuration
+  def self.reset
+    @configuration = Cellgraph::Configuration.new
+  end
+
+  # Configures global settings for CellGraph
+  #   CellGraph.configure do |config|
+  #     config.cellgraph_field = :my_field
+  #   end
+  def self.configure
+    yield(configuration)
+  end
 end
